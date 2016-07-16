21d440dc956eb22cec46248daffa1ffd09a776af
Andrew Jiang <andrew.jiang@bcgdv.com>
2016-07-15 19:04:37 -0700

    begin d3

--- src/d3-waveform.js
+++ src/d3-waveform.js
@@ -1,10 +1,9 @@
 var WaveformData = require('waveform-data');
 var Promise = require('promise');
+var d3 = require('d3');
 
 var d3Waveform = function (path) {
-  getWaveformData(path).then(function (waveform) {
-    console.log(waveform);
-  });
+  getWaveformData(path).then(drawGraph);
 };
 
 global.d3Waveform = d3Waveform;
@@ -24,4 +23,29 @@ function getWaveformData(path) {
 
     xhr.send();
   });
+}
+
+function drawGraph(waveform) {
+  console.log(waveform);
+
+  var svg = d3.select('#vis').append('svg')
+    .attr('width', '1000px')
+    .attr('height', '500px');
+
+  var x = d3.scale.linear();
+  var y = d3.scale.linear();
+  var offsetX = 100;
+
+  x.domain([0, waveform.adapter.length]).rangeRound([0, 1024]);
+  y.domain([d3.min(waveform.min), d3.max(waveform.max)]).rangeRound([offsetX, -offsetX]);
+
+  var area = d3.svg.area()
+    .x(function (d, i) { return x(i); })
+    .y0(function (d, i) { return y(waveform.min[i]); })
+    .y1(function (d, i) { return y(d); });
+
+  svg.append('path')
+    .datum(waveform.max)
+    .attr('transform', function () { return 'translate(0, ' + offsetX + ')'; })
+    .attr('d', area);
 }
